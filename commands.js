455eea7f828db92aea003b776ea2672ade4ce91f
iopdude <775935+iopdude@users.noreply.github.com>
2017-11-10 21:19:55 -0500

    Added join command with a greeting sound file

--- commands.js
+++ commands.js
@@ -1,3 +1,5 @@
+var voiceConnection = null;
+
 module.exports = {
     "ping" : {
         run: function(message) {
@@ -11,5 +13,21 @@ module.exports = {
             message.channel.send("pong2!");
         },
         help: "Second ping because one isn't enough"
+    },
+
+    "join" : {
+        run: function(message) {
+            if (message.member.voiceChannel) {
+                message.member.voiceChannel.join()
+                    .then(function(connection) {
+                        voiceConnection = connection;
+                        message.reply("joined channel");
+                        voiceConnection.playFile('./sound/ohayou.wav');
+                    })
+            } else {
+                message.reply("you need to be in a voice channel");
+            }
+        },
+        help: "Join your voice channel"
     }
 };
